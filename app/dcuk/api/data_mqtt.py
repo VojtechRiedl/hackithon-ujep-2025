236af39d7dbf6bcc76fa5a2e9c632001187bd38c
vojtech riedl <vojtariedl66@gmail.com>
2025-05-24 12:54:44 +0200

    endpoint

--- app/dcuk/api/data_mqtt.py
+++ app/dcuk/api/data_mqtt.py
@@ -5,6 +5,8 @@ from app.xdent.dependencies import SessionDependency
 
 from app.xdent.models.xdent import Data
 
+from fastapi import HTTPException
+
 router = APIRouter(
     prefix="/mqtt",
     tags=["data_mqtt"],
@@ -18,3 +20,20 @@ def get_mqtt_data(session: SessionDependency):
     """
     
     return session.query(Data).all()
+
+
+@router.get("/{id}")
+def get_mqtt_data_by_id(
+    id: int,
+    session: SessionDependency
+):
+    """
+    Get MQTT data.
+    """
+
+    data = session.query(Data).filter(Data.id == id).first()
+
+    if data is None:
+        raise HTTPException(status_code=404, detail="Data not found")
+    
+    return data
